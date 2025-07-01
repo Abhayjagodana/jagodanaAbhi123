bee70ab5639a7fca2d28cd1d3a3a746b9b6fa376
Abhi <jagodanaanhay@gmail.com>
2025-07-01 09:38:59 +0530

    update something

--- src/Pages/SlideBar.tsx
+++ src/Pages/SlideBar.tsx
@@ -42,27 +42,30 @@ const ReviewCard = ({
   username,
   imgTitle,
   body,
-  
-  
 }: {
   img: string;
   name: string;
   username: string;
   body: string;
-  imgTitle: string
+  imgTitle: string;
 }) => {
   return (
     <figure
       className={cn(
         "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-        // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
       )}
     >
-      <div className="flex flex-row items-center gap-2 ">
-        <img className="rounded-full" width="48 " height="32" alt="" src={img} />
+      <div className="flex flex-row items-center gap-2">
+        <img
+          className="rounded-full"
+          width={48}
+          height={32}
+          alt={imgTitle}
+          title={imgTitle}
+          src={img}
+        />
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium dark:text-white">
             {name}
